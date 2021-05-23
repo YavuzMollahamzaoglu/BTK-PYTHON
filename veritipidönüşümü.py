dbb94302de011b20c602c99c7005553d6430c0f8
YavuzMollahamzaoglu <yavuz.mersin@hotmail.com>
2021-05-23 12:51:41 +0300

    veridönüşümü
    veriler örnek ve konu

---
+++ veritipidönüşümü.py
@@ -0,0 +1,33 @@
+# x = input('1.sayı: ')
+# y = input('2. sayı: ')
+
+# print(type(x))
+# print(type(y))
+
+
+# toplam = int(x) + int(y)
+
+# print(toplam)                #ctrl + k + c ile hepsini yorum satırı yapabiliriz
+
+
+
+# x = 5           #int
+# y = 2.5          #float
+
+# name = 'Yavuz'         #sttring
+# isOnline = True         #bool
+
+
+# int to float 
+#  x = float(x)
+#  print(x)
+#  print(type(x))
+
+
+pi = 3.14
+yaricap =  float(input('yarı çap: '))
+alan = pi * (yaricap ** 2)
+cevre = 2 * pi * yaricap
+
+print('alan:', alan)
+print('çevre:', cevre)
