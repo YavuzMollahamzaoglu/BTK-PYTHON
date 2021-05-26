25bcec9938951b5ed4e8f96aa8d210109eddfd17
YavuzMollahamzaoglu <yavuz.mersin@hotmail.com>
2021-05-26 15:53:10 +0300

    fonksiyonuygulaması.py
    fonksiyonlarda uygulama Türkçe part 1

---
+++ fonksiyonuygulama.py
@@ -0,0 +1,19 @@
+'''def yazdir(kelime, adet):
+    print(kelime * adet)
+
+yazdir('Merhaba', 10)'''
+
+
+'''def listeyeCevir(*params):
+    liste =[]
+
+    for param in params :
+        liste.append(param)
+        
+    return liste
+
+result = listeyeCevir(10,20,30, 'Merhaba')
+print(result)'''
+
+
+sayi1 = int(input('sayı1 :')
