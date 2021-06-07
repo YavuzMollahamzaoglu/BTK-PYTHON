c6f70766f6d210683fed9c344848e402b3c10ec7
YavuzMollahamzaoglu <yavuz.mersin@hotmail.com>
2021-06-07 15:28:57 +0300

    Dictionary
    Listeler ve sözlük hakkında herşey

---
+++ dictionary.py
@@ -0,0 +1,40 @@
+#key - value
+
+# 41 => kocaeli 34 => istanbul
+
+'''sehirler = ['kocaeli','istanbul']     #sıralar birbirine uymalı
+plakalar = [41, 34]
+
+
+print(plakalar[sehirler.index('istanbul')])
+
+
+
+#print(plakalar['kocaeli]) => 41 vermesini istiyoruz
+
+plakalar  =  {'mersin' : 33, 'samsun': 55}
+
+print(plakalar['mersin'])
+print(plakalar['samsun'])
+
+
+plakalar['ankara'] = 6
+
+plakalar['rize'] = 53
+print(plakalar)'''
+
+
+users = {
+    'yavuzmolla': {
+        'age' : '19',
+        'address':'mersin',
+        'phone' : '1234567'
+
+    }    
+    
+
+
+}
+
+print(users['yavuzmolla'])
+print(users['yavuzmolla']['age'])
---
+++ tuple.py
@@ -0,0 +1,15 @@
+list = [1,2,3]
+
+tuple = (1, 'iki', 3)
+
+'''print(type(list))
+print(type(tuple))
+
+
+print(tuple[2])
+print(len(tuple))'''
+
+
+tuple = ('molla', 'yavuz')
+
+print(tuple)
